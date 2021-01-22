3d75968270aaacc5bf678d5527bc3341c2a7cfe3
Serhii Chernenko <serhii.c@overdose.digital>
2021-01-22 11:04:52 +0200

    Prettier has been added

--- 5 kyu/Best travel.js
+++
@@ -1,9 +0,0 @@
-// https://www.codewars.com/kata/55e7280b40e1c4a06d0000aa
-
-function chooseBestSum(t, k, ls) {
-    // your code
-}
-
-console.log(chooseBestSum(163, 3, [50, 55, 56, 57, 58])) // 163
-console.log(chooseBestSum(163, 3, [50])) // null
-console.log(chooseBestSum(230, 3, [91, 74, 73, 85, 73, 81, 87])) // 228
