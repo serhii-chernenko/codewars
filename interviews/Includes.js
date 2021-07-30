19dc4d29e5574703beb3ca378e21e14790c37690
Serhii Chernenko <serhii.c@overdose.digital>
2021-07-30 13:18:17 +0300

    remove interview question from the repo

--- interviews/Includes.js
+++
@@ -1,5 +0,0 @@
-const includes = (arr, num) => new Set(arr).has(num);
-
-console.log(includes([1, 3, 8, 10], 8)); // true
-console.log(includes([1, 3, 8, 8, 15], 15)); // true
-console.log(includes([1, 3, 8, 10, 15], 9)); // false
