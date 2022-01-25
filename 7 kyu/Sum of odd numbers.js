d113c9ed1074e05323659094c5bb3a927a106d67
Serhii Chernenko <serhii.c@overdose.digital>
2022-01-25 20:24:38 +0200

    Sum of odd numbers added

---
+++ 7 kyu/Sum of odd numbers.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/javascript
+
+function rowSumOddNumbers(n) {
+    return n ** 3;
+}
+
+rowSumOddNumbers(1); // ?
+rowSumOddNumbers(42); // ?
