437ea60f7af22420fdae5e19d0f852271e11d249
Serhii Chernenko <serhii.c@overdose.digital>
2022-06-02 12:09:42 +0300

    Two to One added

---
+++ 7 kyu/Two to One.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/javascript
+
+function longest(s1, s2) {
+    return [...new Set(s1.concat(s2).split('').sort())].join('');
+}
+
+longest('aretheyhere', 'yestheyarehere');
+const chai = require('chai');
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe('longest', function () {
+    it('Basic tests', function () {
+        assert.strictEqual(longest('aretheyhere', 'yestheyarehere'), 'aehrsty');
+        assert.strictEqual(longest('loopingisfunbutdangerous', 'lessdangerousthancoding'), 'abcdefghilnoprstu');
+        assert.strictEqual(longest('inmanylanguages', 'theresapairoffunctions'), 'acefghilmnoprstuy');
+    });
+});
