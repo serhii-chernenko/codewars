77d9de56f9527ce111dc61acf6dffa84b40e3857
Serhii Chernenko <serhii.c@overdose.digital>
2022-06-13 12:08:18 +0300

    Keep Hydrated added

---
+++ 8 kyu/Keep Hydrated.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript
+
+function litres(time) {
+    return Math.floor(time * 0.5);
+}
+
+const chai = require('chai');
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe('Fixed tests', () => {
+    it('Tests', () => {
+        assert.strictEqual(litres(2), 1, 'should return 1 litre');
+        assert.strictEqual(litres(1.4), 0, 'should return 0 litres');
+        assert.strictEqual(litres(12.3), 6, 'should return 6 litres');
+        assert.strictEqual(litres(0.82), 0, 'should return 0 litres');
+        assert.strictEqual(litres(11.8), 5, 'should return 5 litres');
+        assert.strictEqual(litres(1787), 893, 'should return 893 litres');
+        assert.strictEqual(litres(0), 0, 'should return 0 litres');
+    });
+});
